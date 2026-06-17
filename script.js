3aef38c85da3d8ef9ec3f603d8ad145965ab4057
kundai3 <kundaingwereva3@gmail.com>
2026-06-18 04:04:12 +0800

    Add animateValue function for smooth number animation

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function animateValue(id, target) {
 
+    let current = 0;
+    const increment = target / 100;
+
+    const interval = setInterval(() => {
+
+        current += increment;
+
+        if (current >= target) {
+            document.getElementById(id).innerText = target;
+            clearInterval(interval);
+        } else {
+            document.getElementById(id).innerText = Math.floor(current);
+        }
+
+    }, 20);
+}
+
+animateValue("donorsCount", 500);
+animateValue("requestsCount", 120);
+animateValue("savedCount", 80);
